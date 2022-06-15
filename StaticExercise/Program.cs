dd5ea21bd157a8f057902783c6640c3282e14aa9
Colby Soldan <colbylsoldan@gmail.com>
2022-06-15 10:11:55 -0500

    first commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(30);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(30);
+
+            Console.WriteLine($"Temperature Conversions:" +
+                $"\nCelsius: {celsius}" +
+                $"\nFahrenheit: {fahrenheit}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+
+		public static double FahrenheitToCelsius(double fahrenheit)
+        {
+			return (fahrenheit - 32) * 5/9;
+        }
+
+		public static double CelsiusToFahrenheit(double celsius)
+        {
+			return (celsius * 1.8) + 32;
+        }
+	}
+}
+
